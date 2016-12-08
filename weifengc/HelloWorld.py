83e431c43b64807bf99ecbe82bd3d731f3ad408b
weifengc <bikeprogrammer@gmail.com>
2016-12-07 21:37:28 -0800

    to task7

--- weifengc/HelloWorld.py
+++ weifengc/HelloWorld.py
@@ -23,5 +23,30 @@ while babies < 10:
     parents, babies = (babies, parents + babies)
 
 
+#Task5 - functions
 
+#This is a function, take name as input parameter
+def greet(name):
+    print 'Hello', name
+
+greet('Jack')
+greet('Bob')
+
+
+# Task 6 import and regular expression
+
+import re
+
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print test_string, 'is a valid US local phone number'
+    else:
+        print test_string,'rejected'
+
+# Task 7, dictionaries, generator expression
+
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {'apple' : 1, 'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit] for fruit in my_purchase)
+print 'I owe the grocer $%.2f' % grocery_bill
 
