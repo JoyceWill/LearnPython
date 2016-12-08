c109bd4cf75abbf58a5085dacc93979b9403cba7
Joyce Xu <coxu@saitekiMacBook-Pro.local>
2016-12-07 22:06:48 -0800

    finish task 7

--- coxu/HelloWorld_1207.py
+++ coxu/HelloWorld_1207.py
@@ -19,3 +19,30 @@ while babies < 10:
     parents, babies = (babies, parents + babies)
 
 print'{0}{1}{2}'.format('congyu','like','chouchou')
+
+#Task 5
+def greet(name):
+    print 'Hello', name
+
+greet('Jack')
+greet('Joyce')
+
+# Task 6
+import re
+
+for test_string in ['555-1212','ILL-EGAL','555-12LL']:
+    if(re.match(r'\d{3}-\d{4}$', test_string)):
+        print test_string, 'Is valid'
+    else:
+        print test_string,'not valid'
+
+# Task 7, dictionaries, generator expression
+price = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {'apple': 1, 'banana': 6}
+grocery_bill = sum(price[fruit] * my_purchase[fruit] for fruit in my_purchase)
+print 'I own the grocer $%.2f' % grocery_bill
+
+sum = 0
+for key in my_purchase:
+    sum += my_purchase[key] * price[key]
+print 'my Calculation:',sum
